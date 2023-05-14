b35fe678beba0377fc955bedf3ebc5604f228e3b
Radha743 <worldofnara777@gmail.com>
2023-05-14 12:53:58 +0530

    Next setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen overflow-hidden grid place-content-center">
+      <h1 className="font-bold">Radhey Shyam</h1>
+    </div>
+  )
+}
